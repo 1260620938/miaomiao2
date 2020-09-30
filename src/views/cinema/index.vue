c1c991fbe1cb95b3aa7d410eed58febfe1d6fee8
1260620938 <1260620938@qq.com>
2020-09-30 13:29:52 +0800

    addcreatecomponenttwo

--- src/views/cinema/index.vue
+++ src/views/cinema/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="main">
         <Header title="喵喵影院"></Header>
+        <div id="content">
+            <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont iconxiangxiajiantou"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont iconxiangxiajiantou"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont iconxiangxiajiantou"></i>
+				</div>
+			</div>
+            <cilist />
+        </div>
         <Footer></Footer>
     </div>
 </template>
@@ -9,16 +23,18 @@
 
 import Header from '@/components/Header'
 import Footer from '@/components/TabBar'
+import cilist from '@/components/cilist'
 
 export default {
     name:'cinema',
     components:{
         Header,
-        Footer
+        Footer,
+        cilist
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
 </style>
